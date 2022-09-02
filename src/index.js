4e6d7a7df210be738ffe757754225dbe4c6cb795
Arti <arti.cgd@yandex.com>
2022-09-02 16:00:15 +0300

    основной функционал плюс валидация

--- src/index.js
+++ src/index.js
@@ -1,16 +1,42 @@
-const resultContainer = document.querySelector(".form__result");
 const button = document.querySelector(".form__button")
 const form = document.querySelector(".form__container")
-const inputValue = document.querySelector(".form__input")
+const binInput = document.querySelector(".form__input")
 
 
 
-function handleButton(bin) {
-    console.log("click")
-    resultContainer.textContent = parseInt(bin, 2)
+//РАБОТА КНОПКИ 
+function handleButton() {
+    const bin = binInput.value
+    const resultValue = document.querySelector(".form__result")
+    resultValue.textContent = parseInt(bin, 2)
+    console.log('click');
 }
 
 button.addEventListener("click", handleButton)
+
 form.addEventListener("submit", (e) => {
     e.preventDefault()
 })
+
+//ВАЛИДАЦИЯ ИНПУТА
+function validateImput() {
+    let valid = true
+    const pattern = /[2-9]/
+    const input = binInput.value
+    if (input.length === 0) {
+        console.log("Введите число");
+        valid = false
+    } else if (pattern.test(input)) {
+        valid = false
+        console.log("двоичный код должен быть чел");
+    }
+    if (!valid) {
+        button.setAttribute("disabled", true)
+    } else {
+        button.removeAttribute("disabled")
+    }
+    
+}
+
+// СОБЫТИЯ ИНПУТА
+binInput.addEventListener("input", validateImput)
